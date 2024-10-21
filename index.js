32ec72d4a8b69fc2945cd8987f53ad1feee29595
yyyyyy <youindo10@gmail.com>
2024-10-21 08:07:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bypassLink = require("./fluxus")
+app.get("/", (req, res) => {
+    res.json({ message: "Invalid Endpoint" });
+});
+app.get("/api/fluxus", async (req, res) => {
+    const url = req.query.url;
+    if (url && url.startsWith("https://flux.li/android/external/start.php?HWID=")) {
+        try {
+            const { key, timeTaken } = await bypassLink(url);
+            return res.json({ key, time_taken: timeTaken, credit: "myuko" });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    } else {
+        return res.json({ message: "Please Enter Fluxus Link!" });
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
